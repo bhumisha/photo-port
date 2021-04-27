804b620a37655171948530696fbf787f38d2f256
Bhumisha Dave <bhumishadave@yahoo.com>
2021-04-27 00:33:05 -0700

    final project

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,22 +1,19 @@
+// __tests__/About.test.js
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import About from '..';
 
-// call the cleanup function using the afterEach global function from Jest
 afterEach(cleanup);
-// This will ensure that after each test, we won't have any leftover memory data that could give us false results.
 
 describe('About component', () => {
-    // First Test
-    it('renders', () => {
-        render(<About />);
-      });
-  // Second
-  it('matches snapshot DOM node structure', () => {
-    // render About
+  it('renders', () => {
+    render(<About />);
+  });
+
+  it('matches snapshot', () => {
     const { asFragment } = render(<About />);
+
     expect(asFragment()).toMatchSnapshot();
   });
-  })
-  
+})
